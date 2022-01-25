75050b50c354d606fb3575ca561edc6c1e506d1a
Adonis Jebara <adonisjebara123@gmail.com>
2022-01-25 22:04:23 +0200

    some changes

---
+++ lib/modules/login/login_screen.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:school_app/layout/home_layout.dart';
+import 'package:school_app/shared/components/components.dart';
+import 'package:school_app/shared/components/constants.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    dynamic h = MediaQuery.of(context).size.height;
+    dynamic w = MediaQuery.of(context).size.width;
+
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: bgColor,
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: h * 0.18,
+              ),
+              Text(
+                "مدرستي",
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: mainColor,
+                ),
+              ),
+              SizedBox(
+                height: h * 0.15,
+              ),
+              mainTextFormFields(
+                context: context,
+                labelText: "اسم المستخدم",
+                prefixIcon: Icons.person_outline,
+              ),
+              SizedBox(
+                height: h * 0.045,
+              ),
+              mainTextFormFields(
+                context: context,
+                labelText: "كلمة المرور",
+                suffixIcon: Icons.remove_red_eye_outlined,
+                prefixIcon: Icons.lock_outlined,
+              ),
+              SizedBox(
+                height: h * 0.02,
+              ),
+              Directionality(
+                textDirection: TextDirection.rtl,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      " هل تواجه مشكلة ؟",
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: mainColor),
+                    ),
+                    InkWell(
+                      child: Text(
+                        "تواصل معنا",
+                        style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: mainColor),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: h * 0.03,
+              ),
+              Container(
+                width: w * 0.73,
+                height: 1,
+                color: mainColor,
+              ),
+              SizedBox(
+                height: h * 0.10,
+              ),
+              Material(
+                elevation: 10,
+                shadowColor: mainColor,
+                child: Container(
+                  width: w * 0.90,
+                  height: 60,
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(5)),
+                  child: MaterialButton(
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => HomeLayout(),
+                        ),
+                      );
+                    },
+                    color: mainColor,
+                    child: const Text(
+                      "تسجيل دخول ",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: Colors.white),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/shared/components/components.dart
+++ lib/shared/components/components.dart
@@ -1,36 +1,34 @@
-
 import 'package:flutter/material.dart';
+import 'package:school_app/shared/components/constants.dart';
 
-import 'constants.dart';
-
-
-
-
-Widget mainTextFormFields({context,String ? labelText,IconData ? sufixicon,IconData? prefixIcon})=>
+Widget mainTextFormFields(
+        {context,
+        String? labelText,
+        IconData? suffixIcon,
+        IconData? prefixIcon}) =>
     Directionality(
       textDirection: TextDirection.rtl,
       child: Container(
-        width: MediaQuery.of(context).size.width*0.90,
+        width: MediaQuery.of(context).size.width * 0.90,
         color: Colors.white,
         child: Material(
           elevation: 10,
           shadowColor: Colors.white,
           child: TextFormField(
             decoration: InputDecoration(
-              suffixIcon: Icon(sufixicon,color: main2Color,),
-              prefixIcon: Icon(prefixIcon,color: main2Color,),
-
-
-
-
-
+              suffixIcon: Icon(
+                suffixIcon,
+                color: mainColor,
+              ),
+              prefixIcon: Icon(
+                prefixIcon,
+                color: mainColor,
+              ),
               border: OutlineInputBorder(
                 borderSide: BorderSide.none,
                 borderRadius: BorderRadius.circular(5),
               ),
               labelText: labelText,
-
-
               labelStyle: const TextStyle(
                   color: Color(0xff0D3082),
                   fontSize: 20,
