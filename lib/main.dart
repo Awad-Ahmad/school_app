f6e8ceefe543aa0f94591a527e68721b646a75d7
Ahmad Awad <ahmadmohanasawad@gmail.com>
2022-01-26 11:47:25 +0200

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:school_app/modules/login/login_screen.dart';
+import 'package:school_app/shared/components/constants.dart';
+import 'package:school_app/shared/cubit/AppCubit/AppCubit.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,16 +13,38 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'School App',
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          color: Colors.white,
-          elevation: 14,
+    return BlocProvider(
+      create: (context) => AppCubit(),
+      child: MaterialApp(
+        title: 'School App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          appBarTheme: AppBarTheme(
+            color: Colors.white,
+            elevation: 5,
+            titleTextStyle: TextStyle(
+              color: mainColor,
+              fontSize: 24,
+            ),
+            iconTheme: IconThemeData(
+              color: mainColor,
+            ),
+          ),
+          bottomNavigationBarTheme: BottomNavigationBarThemeData(
+            elevation: 10,
+            selectedLabelStyle: TextStyle(
+              color: mainColor,
+            ),
+            selectedIconTheme: IconThemeData(
+              color: mainColor,
+            ),
+            type: BottomNavigationBarType.fixed,
+            showUnselectedLabels: false,
+          ),
+          primaryColor: mainColor,
         ),
+        home: LoginScreen(),
       ),
-      home: LoginScreen(),
     );
   }
 }
-
