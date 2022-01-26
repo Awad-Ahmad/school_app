1ab9951f3bec8cd1a0f3f250298460c08eb44bb4
Adonis Jebara <adonisjebara123@gmail.com>
2022-01-26 10:53:02 +0200

    nav bar

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -1,26 +1,76 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:school_app/shared/components/constants.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:school_app/shared/cubit/AppCubit/AppCubit.dart';
+import 'package:school_app/shared/cubit/AppCubit/AppStates.dart';
 
 class HomeLayout extends StatelessWidget {
   const HomeLayout({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Directionality(
-      textDirection: TextDirection.rtl,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('مدرستي'),
-          actions: [
-            IconButton(onPressed: (){}, icon:  Icon(Icons.refresh_outlined,color: mainColor,),),
-            IconButton(onPressed: (){}, icon:  Icon(Icons.notifications_none_outlined,color: mainColor,),),
-          ],
-        ),
-        body: Column(
-          children: [],
-        ),
-        bottomNavigationBar: BottomNavigationBar(items: [],),
-      ),
+    return BlocConsumer<AppCubit, AppState>(
+      listener: (BuildContext context, state) {},
+      builder: (BuildContext context, state) {
+        var cubit = AppCubit.get(context);
+        dynamic h = MediaQuery.of(context).size.height;
+        dynamic w = MediaQuery.of(context).size.width;
+
+        return Directionality(
+          textDirection: TextDirection.rtl,
+          child: Scaffold(
+            appBar: AppBar(
+              title: const Text('مدرستي'),
+              actions: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.notifications_none_outlined,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.refresh_outlined,
+                  ),
+                ),
+              ],
+            ),
+            body: Stack(
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      height: h * 0.64,
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(40),
+                          topLeft: Radius.circular(40),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            bottomNavigationBar: BottomNavigationBar(
+              currentIndex: cubit.currentIndex,
+              onTap: cubit.changeIndex,
+              items: const [
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.home_outlined), label: "رئيسي"),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.message_outlined), label: "مراسلة"),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.person_outline), label: "شخصي"),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
