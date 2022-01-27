0453da3919cfe53af8c934a19fc601965e3be9d2
Ahmad Awad <ahmadmohanasawad@gmail.com>
2022-01-27 10:53:34 +0200

    first

--- lib/modules/login/login_screen.dart
+++ lib/modules/login/login_screen.dart
@@ -15,104 +15,106 @@ class LoginScreen extends StatelessWidget {
         resizeToAvoidBottomInset: false,
         backgroundColor: bgColor,
         body: Center(
-          child: Column(
-            children: [
-              SizedBox(
-                height: h * 0.18,
-              ),
-              Text(
-                "مدرستي",
-                style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                  color: mainColor,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: h * 0.18,
                 ),
-              ),
-              SizedBox(
-                height: h * 0.15,
-              ),
-              mainTextFormFields(
-                context: context,
-                labelText: "اسم المستخدم",
-                prefixIcon: Icons.person_outline,
-              ),
-              SizedBox(
-                height: h * 0.045,
-              ),
-              mainTextFormFields(
-                context: context,
-                labelText: "كلمة المرور",
-                suffixIcon: Icons.remove_red_eye_outlined,
-                prefixIcon: Icons.lock_outlined,
-              ),
-              SizedBox(
-                height: h * 0.02,
-              ),
-              Directionality(
-                textDirection: TextDirection.rtl,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      " هل تواجه مشكلة ؟",
-                      style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: mainColor,),
-                    ),
-                    InkWell(
-                      child: Text(
-                        "تواصل معنا",
+                Text(
+                  "مدرستي",
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: mainColor,
+                  ),
+                ),
+                SizedBox(
+                  height: h * 0.15,
+                ),
+                mainTextFormFields(
+                  context: context,
+                  labelText: "اسم المستخدم",
+                  prefixIcon: Icons.person_outline,
+                ),
+                SizedBox(
+                  height: h * 0.045,
+                ),
+                mainTextFormFields(
+                  context: context,
+                  labelText: "كلمة المرور",
+                  suffixIcon: Icons.remove_red_eye_outlined,
+                  prefixIcon: Icons.lock_outlined,
+                ),
+                SizedBox(
+                  height: h * 0.02,
+                ),
+                Directionality(
+                  textDirection: TextDirection.rtl,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        " هل تواجه مشكلة ؟",
                         style: TextStyle(
-                            decoration: TextDecoration.underline,
                             fontSize: 18,
                             fontWeight: FontWeight.bold,
                             color: mainColor,),
                       ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: h * 0.03,
-              ),
-              Container(
-                width: w * 0.73,
-                height: 1,
-                color: mainColor,
-              ),
-              SizedBox(
-                height: h * 0.10,
-              ),
-              Material(
-                elevation: 10,
-                shadowColor: mainColor,
-                child: Container(
-                  width: w * 0.90,
-                  height: 60,
-                  decoration:
-                      BoxDecoration(borderRadius: BorderRadius.circular(5)),
-                  child: MaterialButton(
-                    onPressed: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => HomeLayout(),
+                      InkWell(
+                        child: Text(
+                          "تواصل معنا",
+                          style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: mainColor,),
                         ),
-                      );
-                    },
-                    color: mainColor,
-                    child: const Text(
-                      "تسجيل دخول ",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
-                          color: Colors.white),
-                    ),
+                      ),
+                    ],
                   ),
                 ),
-              )
-            ],
+                SizedBox(
+                  height: h * 0.03,
+                ),
+                Container(
+                  width: w * 0.73,
+                  height: 1,
+                  color: mainColor,
+                ),
+                SizedBox(
+                  height: h * 0.10,
+                ),
+                Material(
+                  elevation: 10,
+                  shadowColor: mainColor,
+                  child: Container(
+                    width: w * 0.90,
+                    height: 60,
+                    decoration:
+                        BoxDecoration(borderRadius: BorderRadius.circular(5)),
+                    child: MaterialButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => HomeLayout(),
+                          ),
+                        );
+                      },
+                      color: mainColor,
+                      child: const Text(
+                        "تسجيل دخول ",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                            color: Colors.white),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
